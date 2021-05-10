4f19a119e7f03fcd60ca3453aa4dc616e1c5ff86
Jimmy Zhang <jimmy.zhang@microsoft.com>
2021-05-10 00:00:08 -0700

    basic app works on local

---
+++ src/app.ts
@@ -0,0 +1,68 @@
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+
+const fetch = require('node-fetch');
+const url = require('url')
+const WELCOME_TEXT = 'Poll App';
+const INFO_TEXT_HEIGHT = 1.2;
+const BUTTON_HEIGHT = 0.6;
+
+export default class Poll {
+	private assets: MRE.AssetContainer;
+
+  private libraryActors: MRE.Actor[] = [];
+
+  private infoText : any;
+
+	constructor(private context: MRE.Context, private params: MRE.ParameterSet) {
+		this.context.onStarted(() => this.started());
+	}
+
+	/**
+	 * Once the context is "started", initialize the app.
+	 */
+	private async started() {
+		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
+		this.assets = new MRE.AssetContainer(this.context);
+
+    this.createInterface();
+	}
+
+  private createInterface(){
+    this.infoText = MRE.Actor.Create(this.context, {
+      actor: {
+        name: 'Info Text',
+        transform: { local: { position: { x: 0, y: INFO_TEXT_HEIGHT, z: 0 } } },
+        collider: { geometry: { shape: MRE.ColliderType.Box, size: { x: 0.5, y: 0.2, z: 0.01 } } },
+        text: {
+          contents: WELCOME_TEXT,
+          height: 0.1,
+          anchor: MRE.TextAnchorLocation.MiddleCenter,
+          justify: MRE.TextJustify.Center
+        }
+      }
+    });
+
+    const helpButton = MRE.Actor.CreateFromLibrary(this.context, {
+      resourceId: 'artifact:1579238405710021245',
+      actor: {
+        name: 'Help Button',
+        transform: { local: { position: { x: 0.35, y: BUTTON_HEIGHT, z: 0 } } },
+        collider: { geometry: { shape: MRE.ColliderType.Box, size: { x: 0.5, y: 0.2, z: 0.01 } } }
+      }
+     });
+    helpButton.setBehavior(MRE.ButtonBehavior).onClick(user => {
+      user.prompt(`
+This app helps you polls users in your Event or World.
+`).then(res => {
+          if(res.submitted){
+            // clicked 'OK'
+          }
+          else
+            this.infoText.text.contents = WELCOME_TEXT;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    });
+  }
+}
