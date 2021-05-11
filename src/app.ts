86e3a626e6f2f82d74e3c5a3214c45c3ef8d7d11
Jimmy Zhang <jimmy.zhang@microsoft.com>
2021-05-11 14:42:11 -0700

    fix debug code

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ const url = require('url')
 const WELCOME_TEXT = 'Poll App';
 const INFO_TEXT_HEIGHT = 1.2;
 const BUTTON_HEIGHT = 0.6;
+const DEBUG = true;
 
 export default class Poll {
 	private assets: MRE.AssetContainer;
@@ -19,6 +20,11 @@ export default class Poll {
 		this.assets = new MRE.AssetContainer(this.context);
 
     this.createInterface();
+
+    if(DEBUG){
+      this.poll('doge');
+      this.poll('Doge?');
+    }
 	}
 
   private createInterface(){
@@ -46,7 +52,7 @@ export default class Poll {
      });
     helpButton.setBehavior(MRE.ButtonBehavior).onClick(user => {
       user.prompt(`
-  This app helps you polls users in your Event or World
+This app helps you polls users in your Event or World
   `).then(res => {
           if(res.submitted){
             // clicked 'OK'
@@ -58,6 +64,42 @@ export default class Poll {
         console.error(err);
       });
     });
+
+    const pollButton = MRE.Actor.CreateFromLibrary(this.context, {
+      resourceId: 'artifact:1579239603192201565', // https://account.altvr.com/kits/1579230775574790691/artifacts/1579239603192201565
+      actor: {
+        name: 'Poll Button',
+        transform: { local: { position: { x: 0, y: BUTTON_HEIGHT, z: 0 } } },
+        collider: { geometry: { shape: MRE.ColliderType.Box, size: { x: 0.5, y: 0.2, z: 0.01 } } }
+      }
+    });
+    pollButton.setBehavior(MRE.ButtonBehavior).onClick(user => {
+      user.prompt(`
+Enter a poll question and click "OK". We'll format it to save you the trouble
+(e.g. 'doge' => 'Doge?').`, true)
+      .then(res => {
+          if(res.submitted && res.text.length > 0){
+            this.poll(res.text);
+          }
+          else{
+            // user clicked 'Cancel'
+          }
+
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    });
+  }
+
+  private poll(q: string){
+    q = q.trim();
+    q = q.charAt(0).toUpperCase() + q.slice(1); // capitalize first letter
+    if(q.charAt(q.length-1) != '?') // stick a question at the end
+      q += '?';
+    this.infoText.text.contents = q;
+    if(DEBUG)
+      console.log(`Polled: ${q}`);
   }
 }
 
